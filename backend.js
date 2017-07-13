89693b16ca3efbb7161904c3483739896f938197
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-07-13 16:05:27 -0400

    math funcitonality and basic styling | WARNING crashes chrome when opening dev tools and js broken?

--- backend.js
+++ backend.js
@@ -4,6 +4,9 @@ var inSideC;
 var outSideA;
 var outSideB;
 var outSideC;
+var outAngleA;
+var outAngleB;
+var outAngleC;
 
 window.onload = function(){
     //load vars
@@ -14,22 +17,39 @@ window.onload = function(){
     outSideA = document.getElementById("outSideA");
     outSideB = document.getElementById("outSideB");
     outSideC = document.getElementById("outSideC");
+
+    outAngleA = document.getElementById("outAngleA");
+    outAngleB = document.getElementById("outAngleB");
+    outAngleC = document.getElementById("outAngleC");
 }
 
 var form = document.getElementById("FormSSS");
 form.addEventListener("submit", function (event){
     event.preventDefault();
+    //form validation
     if(!(inSideA.value && inSideB.value && inSideC.value)){
         alert("Please fill out all fields");
-    }else{
-        var side_a = parseFloat(inSideA.value);
-        var side_b = parseFloat(inSideB.value);
-        var side_c = parseFloat(inSideC.value);
-
-        outSideA.textContent = side_a;
-        outSideB.textContent = side_b;
-        outSideC.textContent = side_c;
+        break;
     }
-
-
-})
+    var side_a = parseFloat(inSideA.value);
+    var side_b = parseFloat(inSideB.value);
+    var side_c = parseFloat(inSideC.value);
+    //math validation
+    if(side_a + side_b >= side_c ||
+       side_b + side_c >= side_a || 
+       side_c + side_a >= side_b){
+        alert("No such Triangle");
+        break;
+    }
+    //math
+    var angle_a = Math.acos((Math.pow(side_b,2) + Math.pow(side_c,2) - Math.pow(side_a,2))/(2 * side_b * side_c));
+    var angle_b = Math.acos((Math.pow(side_a,2) + Math.pow(side_c,2) - Math.pow(side_b,2))/(2 * side_a * side_c));
+    var angle_c = 180 - angle_a - angle_b;
+    //output
+    outSideA.textContent = side_a;
+    outSideB.textContent = side_b;
+    outSideC.textContent = side_c;
+    outAngleA.textContent = angle_a;
+    outAngleB.textContent = angle_b;
+    outAngleC.textContent = angle_c;
+});
